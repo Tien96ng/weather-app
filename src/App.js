e4a85c7e1a5f127977f5ee4cc094d7f83fb62755
tien96ng <tien96ng@gmail.com>
2020-12-23 17:21:44 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useContext, useEffect, useRef } from 'react'
+
 
 function App() {
+  const [weather, setWeather] = useState('');
+  const [location, setLocation] = useState('')
+
+  useEffect(() => {
+    
+  
+
+  // getWeatherAPI()
+
+  /*
+  const timer = setInterval(() => {
+    getWeatherAPI()
+    console.log('5 Seconds!')
+  }, 5000);
+  
+  return () => clearInterval(timer) 
+  */
+  }, []);
+
+  async function getWeatherAPI() {
+    try {
+      const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=fa717a930bec03ce7d0d693a9cd2c54b`)
+      const weatherData = await response.json()
+      setWeather(weatherData)
+      console.log(weather)
+    } catch (error) {
+      console.log(error);
+  }
+}
+
+
+
+  const input = (e) => {
+    if(e.key === "Enter") {
+      setLocation(e.target.value)
+      getWeatherAPI()
+    } else {
+      console.log(e.target.value)
+    }
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World </h1> <br />
+      <input 
+        value={location}
+        placeholder={"search Location"}
+        onChange={(e) => setLocation(e.target.value)}
+        onKeyDown={input}
+      />
+
+
     </div>
   );
 }
